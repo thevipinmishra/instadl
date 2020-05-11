f542ba5e4281532b5331be61e61c91c87b200429
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-11 17:25:41 +0530

    made a function for finding the display url of the image to download

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('It Works');
+const axios = require('axios').default;
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("url > ", function(url) {
+	urls = url.split("?")
+	console.log(urls[0]+"?__a=1")
+	axios.get(urls[0]+"?__a=1")
+		.then(function (response) {
+			console.log(response.data["graphql"]["shortcode_media"]["display_url"]);
+			console.log(response.status);
+			console.log(response.statusText);
+	});
+	rl.close();
+});
