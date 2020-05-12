6ad5db84c90b09fb37c070add388aeef30c3afab
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-12 17:43:11 +0530

    now no need of app.js and now gives direct link og content

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 console.log('It Works');
 const axios = require('axios').default;
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question("url > ", function(url) {
-	urls = url.split("?")
-	console.log(urls[0]+"?__a=1")
-	axios.get(urls[0]+"?__a=1")
-		.then(function (response) {
-			if (response.data["graphql"]["shortcode_media"]["is_video"]) {
-				console.log(response.data["graphql"]["shortcode_media"]["video_url"]);
-			} else {
-				console.log(response.data["graphql"]["shortcode_media"]["display_url"]);
-			};
-			console.log(response.status);
-			console.log(response.statusText);
-	});
-	rl.close();
-});
+async function retrieve(url) {
+	var urls = url.split("?");
+	var resp = await axios.get(urls[0]+"?__a=1");
+	if (resp.data["graphql"]["shortcode_media"]["is_video"]) {
+		return resp.data["graphql"]["shortcode_media"]["video_url"];
+	} else {
+		return resp.data["graphql"]["shortcode_media"]["display_url"]);
+	};
+};
+
+let x = retrieve("https://www.instagram.com/p/B_-V0rilBwo/?igshid=ep45f75tyki9");
+console.log(x);
